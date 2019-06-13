b67061de64a999fc92ca26e5ebbe34120e6ebd1d
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-06-13 15:34:01 -0400

    Added an array of all alphabet letters to javascript file. created initial variables.

---
+++ assets/javascript/javascript.js
@@ -0,0 +1,12 @@
+//declare an array that contains each letter of the alphabet
+var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"]
+
+var userChoice;
+var userScore;
+var computerSore;
+var computerGuess;
+var computerScore;
+var guessesLeft;
+var guessesSoFar;
+
+
