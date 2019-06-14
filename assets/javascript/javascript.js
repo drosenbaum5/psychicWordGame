1a80586954a5dd1c6d14d9d702d16686d9ba66d7
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-06-14 01:00:07 -0400

    created additional variables for holding and pushing text content to HTML document through JavaScript. Successfully displays Wins, Losses, Guesses Left, and Guesses so far values.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -7,14 +7,32 @@ var userChoice;
 var wins = 0;
 var losses = 0;
 var computerGuess;
-var computerScore;
+var computerScore=0;
+var numberOfGuesses=0;
+var guessesRemaining = 9;
 
 
 
 //create variables to hold references to HTML Element IDs so we can display wins, losses, guesses left, and guesses to far
 
-var wins = document.getElementById("user-wins");
-var losses = document.getElementById("user-losses");
+var winsText = document.getElementById("user-wins");
+var lossesText = document.getElementById("user-losses");
 var guessesLeft = document.getElementById("guesses-left");
 var guessesSoFar = document.getElementById("guesses-so-far");
 
+
+ // Randomly chooses a choice from the options array. This is the Computer's guess.
+//  var computerGuess = computerChoices[Math.floor(Math.random() * computerChoices.length)];
+
+
+
+
+
+
+
+
+//Writes the content of the variables to the corresponding element ID's
+ winsText.textContent = "Wins: " + wins;
+ lossesText.textContent = "Losses: " + losses;
+ guessesLeft.textContent = "Guesses left: " + guessesRemaining;
+ guessesSoFar.textContent = "Guesses so far: " + numberOfGuesses;
