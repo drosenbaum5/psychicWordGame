59ed42ebc4b82e7dfaebf4ff56e9f08cb1146ce3
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-06-13 21:09:33 -0400

    added ID's to all tags that will display values. Removed some initial variables and created new variables with document.GetElementByID values in js file. Added initial test variable for computer guess.

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -1,12 +1,20 @@
 //declare an array that contains each letter of the alphabet
 var alphabet = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","x","y","z"]
 
+
+//Variables to hold wins, losses, guesses left, and guesses so far 
 var userChoice;
-var userScore;
-var computerSore;
+var wins = 0;
+var losses = 0;
 var computerGuess;
 var computerScore;
-var guessesLeft;
-var guessesSoFar;
 
+
+
+//create variables to hold references to HTML Element IDs so we can display wins, losses, guesses left, and guesses to far
+
+var wins = document.getElementById("user-wins");
+var losses = document.getElementById("user-losses");
+var guessesLeft = document.getElementById("guesses-left");
+var guessesSoFar = document.getElementById("guesses-so-far");
 
