9e2321ac050d88c12f0f138208488aa9997ee3e7
Dan Rosenbaum <drosenbaum5@gmail.com>
2019-06-14 22:17:45 -0400

    updated js file to include additional changes to counters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,6 +12,13 @@ var guessesRemaining = 9;
 
 
 
+
+var nextGame = function() {
+guessesRemaining = 9;
+numberOfGuesses = [];
+
+}
+
 //create variables to hold references to HTML Element IDs so we can display wins, losses, guesses left, and guesses to far
 
 var winsText = document.getElementById("user-wins");
@@ -20,50 +27,25 @@ var guessesLeft = document.getElementById("guesses-left");
 var guessesSoFar = document.getElementById("guesses-so-far");
 
 
-
-
-//Triggers the start of the game upon a key press. Pressing a key causes the counters to appear
 document.onkeyup = function(event) {
 
-    var userChoice = event.key;
+    var userChoice = String.fromCharCode(event.keyCodeevent).toLowerCase();
     numberOfGuesses.push(userChoice);
 
-    if(numberOfGuesses.length === 10 ) {
-
-
-        numberOfGuesses = [];
-    }
-
-
-// Randomly chooses a choice from the options array. This is the Computer's guess.
+    // Randomly chooses a choice from the options array. This is the Computer's guess.
     var computerGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 
+    
 
-//compares the users guess against the computers guess. If the user wins, win counter will go up by one. if the user loses    
-if(userChoice === computerGuess){
 
-  wins++
- 
-
-}
-
-else {
-
-    guessesRemaining--
-
-    if(guessesRemaining === 0){
-        losses++
-        guessesRemaining = 9;
-    }
-}
 
 
 
 //Writes the content of the variables to the corresponding element ID's
- winsText.textContent = "Wins: " + wins;
- lossesText.textContent = "Losses: " + losses;
- guessesLeft.textContent = "Guesses left: " + guessesRemaining;
- guessesSoFar.textContent = "Guesses so far: " + numberOfGuesses;
+
+lossesText.textContent = "Losses: " + losses;
+guessesLeft.textContent = "Guesses left: " + guessesRemaining;
+guessesSoFar.textContent = "Guesses so far: " + numberOfGuesses;
+winsText.textContent = "Wins: " + wins;
+
 }
-
-
